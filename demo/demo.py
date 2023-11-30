cd0bdea2baf793c2be06a19685635149bc9cd30f
Racha Varun <83562569+varunGi@users.noreply.github.com>
2023-11-30 11:11:06 +0530

    Add files via upload

--- demo/demo.py
+++ demo/demo.py
@@ -1,2 +1,11 @@
 import streamlit as st
 st.write("Hello world")
+st.markdown(
+    """
+    <script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-5056338602918094"
+     crossorigin="anonymous"></script>
+
+
+    """,
+    unsafe_allow_html=True
+)
