cb94b8e81fa37b68be8e224324e07c1add4276bb
Racha Varun <83562569+varunGi@users.noreply.github.com>
2023-11-30 17:47:38 +0530

    Update5 demo.py

--- demo/demo.py
+++ demo/demo.py
@@ -3,15 +3,11 @@ import streamlit as st
 st.title("My Streamlit App")
 
 adsense_code = """
-    <head>
+    
         <script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-5056338602918094"
          crossorigin="anonymous"></script>
         <!-- Your AdSense ad unit code goes here -->
-    </head>
-    <body>
-        <h1>Hello Streamlit!</h1>
-        <!-- Rest of your Streamlit app content goes here -->
-    </body>
+   
 """
 st.write(adsense_code, allow_html=True)
 st.write("Hello world")
