42e30ef54a9f87d058fc722b495d75d86e223bc0
Racha Varun <83562569+varunGi@users.noreply.github.com>
2023-11-30 15:05:38 +0530

    Update ads demo.py

--- demo/demo.py
+++ demo/demo.py
@@ -3,11 +3,20 @@ import streamlit.components.v1 as components
 st.write("Hello world")
 components.html(
     """
-    <head>
-    <script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-5056338602918094"
+    <body>
+   <script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-5056338602918094"
      crossorigin="anonymous"></script>
-
-    </head>
+<!-- ad1 -->
+<ins class="adsbygoogle"
+     style="display:block"
+     data-ad-client="ca-pub-5056338602918094"
+     data-ad-slot="3075916015"
+     data-ad-format="auto"
+     data-full-width-responsive="true"></ins>
+<script>
+     (adsbygoogle = window.adsbygoogle || []).push({});
+</script>
+    </body>
     """
    
 )
