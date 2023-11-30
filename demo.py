f7dc1e807bbb3529b7563e95f05bd857c2d6cd02
Racha Varun <83562569+varunGi@users.noreply.github.com>
2023-11-30 13:23:24 +0530

    Update demo.py

--- demo.py
+++ demo.py
@@ -1,12 +1,12 @@
 import streamlit as st
 st.write("Hello world")
-st.markdown(
-    """
-    <script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-5056338602918094"
-     crossorigin="anonymous"></script>
+# st.markdown(
+#     """
+#     <script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-5056338602918094"
+#      crossorigin="anonymous"></script>
 
 
-    """,
-    unsafe_allow_html=True
-)
-st.write("Complete")
+#     """,
+#     unsafe_allow_html=True
+# )
+st.write("Complete DONE!")
