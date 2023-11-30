31c5df9f99211b12b7a617925b4f5e4df023c52a
Racha Varun <83562569+varunGi@users.noreply.github.com>
2023-11-30 17:26:03 +0530

    Update demo.py

--- demo/demo.py
+++ demo/demo.py
@@ -1,23 +1,5 @@
 import streamlit as st
 import streamlit.components.v1 as components
 st.write("Hello world")
-components.html(
-    """
-    <body>
-   <script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-5056338602918094"
-     crossorigin="anonymous"></script>
-<!-- ad1 -->
-<ins class="adsbygoogle"
-     style="display:block"
-     data-ad-client="ca-pub-5056338602918094"
-     data-ad-slot="3075916015"
-     data-ad-format="auto"
-     data-full-width-responsive="true"></ins>
-<script>
-     (adsbygoogle = window.adsbygoogle || []).push({});
-</script>
-    </body>
-    """
-   
-)
-st.write("over")
+st.javascript("""<script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-5056338602918094" crossorigin="anonymous"></script> """)
+st.write("over end")
